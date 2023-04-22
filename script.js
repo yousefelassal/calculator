982a896c66efbb134b1403342b1da72d71679a07
yousef <yousefsameh24@gmail.com>
2023-04-22 11:18:09 +0200

    update style, add fonts

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const buttons = document.querySelectorAll('.button');
-let screenCurrent = document.querySelector('.screen__output--current');
-let screenPrevious = document.querySelector('.screen__output--previous');
+let screenOutput = document.querySelector('.screen__output');
 
 // buttons.forEach(button => {
 //     button.addEventListener('click', () => {
@@ -21,6 +20,8 @@ let divide = (a, b) =>{
         return a / b;
     }
 };
+let percent = (a) => a / 100;
+let negate = (a) => a * -1;
 
 let operate = (operator, a, b) => {
     switch(operator){
@@ -32,6 +33,10 @@ let operate = (operator, a, b) => {
             return multiply(a, b);
         case '/':
             return divide(a, b);
+        case '%':
+            return percent(a);
+        case '+/-':
+            return negate(a);
     }
 }
 
@@ -39,26 +44,27 @@ let previousValue = 0;
 let currentValue = 0;
 let operator = '';
 
-buttons.forEach(button => {
-    button.addEventListener('click', () => {
-        if(button.dataset.type === 'number'){
-            if(screenCurrent.textContent === '0'){
-                screenCurrent.textContent = button.dataset.value;
-            } else {
-                screenCurrent.textContent += button.dataset.value;
-            }
-        } else if(button.dataset.type === 'operator'){
-            operator = button.dataset.value;
-            previousValue = Number(screenCurrent.textContent);
-            screenPrevious.textContent = `${previousValue} ${operator}`;
-            screenCurrent.textContent = '0';
-        } else if(button.dataset.type === 'equal'){
-            currentValue = Number(screenCurrent.textContent);
-            screenCurrent.textContent = operate(operator, previousValue, currentValue);
-            screenPrevious.textContent = '';
-        } else if(button.dataset.type === 'clear'){
-            screenCurrent.textContent = '0';
-            screenPrevious.textContent = '';
-        }
-    });
-});
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         if(button.dataset.type === 'number'){
+//             if(screenCurrent.textContent === '0'){
+//                 screenCurrent.textContent = button.dataset.value;
+//             } else {
+//                 screenCurrent.textContent += button.dataset.value;
+//             }
+//         } else if(button.dataset.type === 'operator'){
+//             operator = button.dataset.value;
+//             previousValue = Number(screenCurrent.textContent);
+//             screenPrevious.textContent = `${previousValue} ${operator}`;
+//             screenCurrent.textContent = '0';
+//         } else if(button.dataset.type === 'equal'){
+//             currentValue = Number(screenCurrent.textContent);
+//             screenCurrent.textContent = operate(operator, previousValue, currentValue);
+//             screenPrevious.textContent = '';
+//         } else if(button.dataset.type === 'clear'){
+//             screenCurrent.textContent = '0';
+//             screenPrevious.textContent = '';
+//         }
+//     });
+// });
